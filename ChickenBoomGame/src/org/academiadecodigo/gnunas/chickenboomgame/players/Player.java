490d346758927ccb61efc1ce69bb91f9837265b0
Francisco Borges <afgcsborges@gmail.com>
2020-10-22 12:48:47 +0100

    Player super class nopw has common methods to both chickens.

--- ChickenBoomGame/src/org/academiadecodigo/gnunas/chickenboomgame/players/Player.java
+++ ChickenBoomGame/src/org/academiadecodigo/gnunas/chickenboomgame/players/Player.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.gnunas.chickenboomgame.players;
 
 import org.academiadecodigo.gnunas.chickenboomgame.Field;
 import org.academiadecodigo.gnunas.chickenboomgame.Positionable;
+import org.academiadecodigo.gnunas.chickenboomgame.gameobjects.Movement;
 import org.academiadecodigo.gnunas.chickenboomgame.gameobjects.SteroidType;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
@@ -11,11 +12,18 @@ public abstract class Player implements Positionable {
     private Picture chicken;
     private boolean crashed;
     private SteroidType steroidType;
+    private SteroidType status;
+    private int speed;
+    private boolean moving ;
+    private Movement direction;
 
     public Player(Field field, Picture chicken) {
         this.field = field;
         this.chicken = chicken;
         crashed = false;
+        speed = 5;
+        status = SteroidType.CORN;
+        direction = Movement.DOWN;
 
     }
 
@@ -78,6 +86,72 @@ public abstract class Player implements Positionable {
         show();
     }
 
+    public void setStatus(SteroidType type) {
+        status = type;
+        switch (type) {
+            case ICE:
+                speed = (int) (speed * 0.5);
+                break;
 
+            case CORN:
+                speed = (int) (speed * 1.5);
+                break;
+
+            case MUSHROOM:
+                int random = (int) (Math.random()*2);
+                if( random == 1) {
+                    getChicken().grow(10, 10);
+                    break;
+                }
+                getChicken().grow(-10,-10);
+                break;
+        }
+    }
+
+    public void move() {
+
+        if (reachedEdge()) {
+            moving = false;
+        }
+        if (moving) {
+            if (status == SteroidType.SKULL) {
+                getChicken().translate(direction.x * -speed, direction.y * -speed);
+                return;
+            }
+            getChicken().translate(direction.x * speed, direction.y * speed);
+        }
+    }
+
+
+    public void moveInDirection(Movement direction){
+        this.direction = direction;
+        refreshImage();
+        moving = true;
+    }
+
+    protected Movement getDirection() {
+        return direction;
+    }
+
+    public void stopMoving()
+    {
+        moving = false;
+    }
+
+    private boolean reachedEdge(){
+
+        if(status == SteroidType.SKULL){
+            int nextX = getChicken().getX()-direction.x*speed;
+            int nextY = getChicken().getY()-direction.y*speed;
+            return  !((nextX >=120 && nextX+getChicken().getWidth() < getField().getWidth()-120)
+                    && (nextY >=120 && nextY+getChicken().getHeight() < getField().getHeight()-120));
+        }
+        int nextX = getChicken().getX()+direction.x*speed;
+        int nextY = getChicken().getY()+direction.y*speed;
+        return !((nextX >=120 && nextX+getChicken().getWidth() < getField().getWidth()-120)
+                && (nextY >=120 && nextY+getChicken().getHeight() < getField().getHeight()-120));
+    }
+
+    protected abstract void refreshImage();
 }
 
--- ChickenBoomGame/src/org/academiadecodigo/gnunas/chickenboomgame/players/WhiteChicken.java
+++ ChickenBoomGame/src/org/academiadecodigo/gnunas/chickenboomgame/players/WhiteChicken.java
@@ -1,89 +1,17 @@
 package org.academiadecodigo.gnunas.chickenboomgame.players;
 
 import org.academiadecodigo.gnunas.chickenboomgame.Field;
-import org.academiadecodigo.gnunas.chickenboomgame.gameobjects.Movement;
-import org.academiadecodigo.gnunas.chickenboomgame.gameobjects.SteroidType;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class WhiteChicken extends Player {
 
-    private boolean moving ;
-    private Movement direction;
-    private SteroidType status;
-    private int speed;
-
     public WhiteChicken(Field field) {
 
         super(field, new Picture(field.getWidth()/3f,field.getHeight()/2f,"resources/images/whitechicken/down/whitechickendown.png"));
-        status = SteroidType.CORN;
-        this.speed = 5;
-        this.direction = Movement.DOWN;
     }
 
-
-
-    public void setStatus(SteroidType type) {
-        status = type;
-        switch (type) {
-            case ICE:
-                speed = (int) (speed * 0.5);
-                break;
-
-            case CORN:
-                speed = (int) (speed * 1.5);
-                break;
-
-            case MUSHROOM:
-                int random = (int) (Math.random()*2);
-                if( random == 1) {
-                    getChicken().grow(10, 10);
-                    break;
-                }
-                getChicken().grow(-10,-10);
-                break;
-        }
-    }
-
-    public void move() {
-
-        if(reachedEdge()){
-            moving = false;
-        }
-        if (moving){
-            if (status == SteroidType.SKULL){
-                getChicken().translate(direction.x*-speed,direction.y*-speed);
-                return;
-            }
-            getChicken().translate(direction.x*speed,direction.y*speed);
-        }
-    }
-
-    public void moveInDirection(Movement direction){
-        this.direction = direction;
-        refreshImage();
-        moving = true;
-    }
-
-    public void stopMoving()
-    {
-        moving = false;
-    }
-
-    private boolean reachedEdge(){
-        if(status == SteroidType.SKULL){
-            int nextX = getChicken().getX()-direction.x*speed;
-            int nextY = getChicken().getY()-direction.y*speed;
-            return  !((nextX >=120 && nextX+getChicken().getWidth() < getField().getWidth()-120)
-                    && (nextY >=120 && nextY+getChicken().getHeight() < getField().getHeight()-120));
-        }
-        int nextX = getChicken().getX()+direction.x*speed;
-        int nextY = getChicken().getY()+direction.y*speed;
-        return !((nextX >=120 && nextX+getChicken().getWidth() < getField().getWidth()-120)
-                && (nextY >=120 && nextY+getChicken().getHeight() < getField().getHeight()-120));
-    }
-
-    private void refreshImage(){
-        switch (direction){
+    protected void refreshImage(){
+        switch (getDirection()){
             case UP:
                 getChicken().load("resources/images/whitechicken/up/whitechickenup.png");
                 break;
@@ -98,5 +26,7 @@ public class WhiteChicken extends Player {
                 break;
         }
     }
+
+
 
 }
